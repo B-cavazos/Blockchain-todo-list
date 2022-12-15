107b131ea4eeed5f9085e61730c12c297998b350
Brittney Cavazos <brittneycavazos@Brittneys-MBP.lan>
2022-12-15 07:53:01 -0800

    built todo smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,24 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
   uint public taskCount;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+constructor() public{
+    //default task
+    createTask("Take a walk around the block");
+}
+
+  function createTask(string memory _content) public {
+    //puts task in the mapping
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
 }
